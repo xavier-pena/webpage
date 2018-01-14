32105a1bac79a431dad1b4f82382868d80b54753
Your Name <you@example.com>
2018-01-14 04:15:31 -0500

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule , Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ShowcaseComponent } from './components/showcase/showcase.component';
+import { NewsletterComponent } from './components/newsletter/newsletter.component';
+import { BoxesComponent } from './components/boxes/boxes.component';
 
+
+const appRoutes: Routes = [
+  {path: '', component: HeaderComponent},
+  {path: '', component: ShowcaseComponent},
+  {path: '', component: NewsletterComponent},
+  {path: '', component: BoxesComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShowcaseComponent,
+    NewsletterComponent,
+    BoxesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
